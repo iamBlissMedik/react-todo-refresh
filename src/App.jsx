b8b17109f5314d2ef5ffbdf36616031ebd41a358
Blizz <ellamsbliss@gmail.com>
2024-01-06 07:40:53 +0100

    react icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { AiOutlineDelete } from "react-icons/ai";
+import { BsCheckLg } from "react-icons/bs";
 import "./App.css";
 function App() {
   const [isCompleteScreen, SetIsCompleteScreen] = useState(false);
@@ -42,6 +44,10 @@ function App() {
             <div className="todo-list-item">
               <h3>task one</h3>
               <p>description</p>
+              <div>
+                <AiOutlineDelete className="delete-icon" />
+                <BsCheckLg className="check-icon"/>
+              </div>
             </div>
           </div>
         </div>
