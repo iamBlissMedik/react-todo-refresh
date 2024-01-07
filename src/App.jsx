650299c348ffe0669f8e040762046bdf4b016296
Blizz <ellamsbliss@gmail.com>
2024-01-07 19:31:14 +0100

    keep the streak going

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,19 @@ import { AiOutlineDelete } from "react-icons/ai";
 import { BsCheckLg } from "react-icons/bs";
 import "./App.css";
 function App() {
-  const [isCompleteScreen, SetIsCompleteScreen] = useState(false);
+  const [isCompleteScreen, setIsCompleteScreen] = useState(false);
+  const [allTodos, setAllTodos] = useState([]);
+  const [newTitle, setNewTitle] = useState("");
+  const [newDescription, setNewDescription] = useState("");
+  const handleAddTodo = () => {
+    let newTodoItem = {
+      title: newTitle,
+      description: newDescription,
+    };
+    let updatedTodoArr = [...allTodos, newTodoItem];
+   
+    setAllTodos(updatedTodoArr);
+  };
   return (
     <>
       <div className="app">
@@ -12,43 +24,60 @@ function App() {
           <div className="todo-input">
             <div className="todo-input-item">
               <label htmlFor="">Title</label>
-              <input type="text" placeholder="what's the task title?" />
+              <input
+                type="text"
+                value={newTitle}
+                onChange={(e) => setNewTitle(e.target.value)}
+                placeholder="what's the task title?"
+              />
             </div>
             <div className="todo-input-item">
               <label htmlFor="">description</label>
-              <input type="text" placeholder="what's the task description?" />
+              <input
+                type="text"
+                value={newDescription}
+                onChange={(e) => setNewDescription(e.target.value)}
+                placeholder="what's the task description?"
+              />
             </div>
             <div className="todo-input-item">
-              <button type="button" className="primaryButton">
+              <button
+                type="button"
+                onClick={handleAddTodo}
+                className="primaryButton"
+              >
                 Add
               </button>
             </div>
           </div>
           <div className="btn-area">
             <button
-              className={`secondaryButton ${
-                isCompleteScreen === false && "active"
-              }`}
-              onClick={() => SetIsCompleteScreen(false)}
+              className={`secondaryButton ${!isCompleteScreen && "active"}`}
+              onClick={() => setIsCompleteScreen(false)}
             >
               Todo
             </button>
             <button
               className={`secondaryButton ${isCompleteScreen && "active"}`}
-              onClick={() => SetIsCompleteScreen(true)}
+              onClick={() => setIsCompleteScreen(true)}
             >
               Completed
             </button>
           </div>
           <div className="todo-list">
-            <div className="todo-list-item">
-              <h3>task one</h3>
-              <p>description</p>
-              <div>
-                <AiOutlineDelete className="delete-icon" />
-                <BsCheckLg className="check-icon"/>
-              </div>
-            </div>
+            {allTodos.map((todo, index) => {
+              return (
+                <div className="todo-list-item" key={index}>
+                  {" "}
+                  <h3>{todo.title}</h3>
+                  <p>{todo.description}</p>
+                  <div>
+                    <AiOutlineDelete className="delete-icon" />
+                    <BsCheckLg className="check-icon" />
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
